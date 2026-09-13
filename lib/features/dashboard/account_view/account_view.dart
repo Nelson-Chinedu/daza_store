b38e6023c4a424afd68bd7f2d0e0a52f15ef519a
Nelson-Chinedu <egwuchinedu69@gmail.com>
2026-09-13 14:06:12 +0100

    feat: logout modal

--- lib/features/dashboard/account_view/account_view.dart
+++ lib/features/dashboard/account_view/account_view.dart
@@ -1,6 +1,7 @@
 import 'package:daza_store_commerce/features/dashboard/account_view/account_viewmodel.dart';
 import 'package:daza_store_commerce/features/dashboard/orders/orders_view.dart';
 import 'package:daza_store_commerce/features/dashboard/payment_method/payment_method_view.dart';
+import 'package:daza_store_commerce/shared/widgets/button/button_view.dart';
 import 'package:daza_store_commerce/styles/brand_color.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
@@ -71,40 +72,29 @@ class AccountView extends StatelessWidget {
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
                   ),
                   const SizedBox(height: 20),
-                  accountSetup(
-                    name: 'Your profile',
-                    path: '',
-                    icon: Icons.person,
-                  ),
-                  accountSetup(
-                    name: 'Manage Address',
-                    path: '',
-                    icon: Icons.house,
-                  ),
+                  accountSetup(name: 'Your profile', icon: Icons.person),
+                  accountSetup(name: 'Manage Address', icon: Icons.house),
                   accountSetup(
                     name: 'Payment Methods',
-                    path: '',
+
                     icon: Icons.payment,
                     onTap: () =>
                         NavigationService().navigateToView(PaymentMethodView()),
                   ),
                   accountSetup(
                     name: 'My Orders',
-                    path: '',
+
                     icon: Icons.shopping_bag,
                     onTap: () =>
                         NavigationService().navigateToView(OrdersView()),
                   ),
+                  accountSetup(name: 'My Coupons', icon: Icons.card_giftcard),
+                  accountSetup(name: 'My Wallet', icon: Icons.wallet),
+                  accountSetup(name: 'Settings', icon: Icons.settings),
                   accountSetup(
-                    name: 'My Coupons',
-                    path: '',
-                    icon: Icons.card_giftcard,
-                  ),
-                  accountSetup(name: 'My Wallet', path: '', icon: Icons.wallet),
-                  accountSetup(
-                    name: 'Settings',
-                    path: '',
-                    icon: Icons.settings,
+                    name: 'Logout',
+                    icon: Icons.logout,
+                    onTap: () => _showLogoutBottomSheet(context),
                   ),
                 ],
               ),
@@ -118,7 +108,6 @@ class AccountView extends StatelessWidget {
   Widget accountSetup({
     required String name,
     required dynamic icon,
-    required String path,
     VoidCallback? onTap,
   }) {
     return GestureDetector(
@@ -159,6 +148,72 @@ class AccountView extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  void _showLogoutBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.all(24),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24),
+              topRight: Radius.circular(24),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 80,
+                height: 4,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(2),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              const Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Divider(color: Colors.grey.shade200, height: 1),
+              const SizedBox(height: 24),
+
+              Text(
+                'Are you sure you want to log out?',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey.shade600,
+                ),
+              ),
+              const SizedBox(height: 28),
+
+              Row(
+                spacing: 20,
+                children: [
+                  Expanded(child: Button(label: 'Cancel')),
+
+                  Expanded(child: Button(label: 'Yes, Logout')),
+                ],
+              ),
+              const SizedBox(height: 12),
+            ],
+          ),
+        );
+      },
     );
   }
 }
